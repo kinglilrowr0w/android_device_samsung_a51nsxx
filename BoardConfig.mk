52cab86be0a04bbe008d7b86ce9dda01f2a571bf
Aaron Doe <56174274+kinglilrowr0w@users.noreply.github.com>
2020-05-11 23:06:36 +0100

    a51ns: Configure TARGET_2ND_ARCH to match device spec
    We have 4 cortex-a53 cores as well as 4 cortex-a73 cores, update board flags to correlate this.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,11 +16,11 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 # Secondary Architecture
-TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH := arm64
 TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_ABI := arm64-v8a
+TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
